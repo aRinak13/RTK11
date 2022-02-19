209a0bc14eb4612e2de4d63568ea562544ecc9c0
grishinaip <irina.p.grishina@rt.ru>
2022-02-19 14:11:30 +0300

    fix: Считает кол-во миль от стоимости билета.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int saleTicket = 1500;
+        int oneMileTicketSpend = 20;
+        int totalMile = saleTicket / oneMileTicketSpend;
+        System.out.println(totalMile);
+    }
+}
